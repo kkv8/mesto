1904eb4864feab687ea0379bd3e5031664f7a735
kkv <ekaterinav782@gmail.com>
2023-07-23 13:10:55 +0300

    js fixed

--- scripts/index.js
+++ scripts/index.js
@@ -1,70 +1,67 @@
-let editButton = document.querySelector('.profile__edit-button');
-let popupEdit = document.querySelector('.popup_type_edit-profile')
-let popupAdd = document.querySelector('.popup_type_add-card')
-let popupProfileName = document.querySelector('.popup__input_type_name')
-let profileName = document.querySelector('.profile__name')
-let popupAbout = document.querySelector('.popup__input_type_about')
-let profileDescription = document.querySelector('.profile__description')
-let editForm = document.querySelector('.edit-form')
-const addButton = document.querySelector('.profile__add-button')
+const popupEditButton = document.querySelector('.profile__edit-button');
+const popupEditProfile = document.querySelector('.popup_type_edit-profile')
+const popupAddCard = document.querySelector('.popup_type_add-card')
+const inputProfileName = document.querySelector('.popup__input_type_name')
+const profileName = document.querySelector('.profile__name')
+const inputAbout = document.querySelector('.popup__input_type_about')
+const profileDescription = document.querySelector('.profile__description')
+const formEdit = document.querySelector('.edit-form')
+const buttonAdd = document.querySelector('.profile__add-button')
 
 const initialCards = [
   {
     name: 'Гас и Флейк',
-    link: 'https://ltdfoto.ru/images/2023/07/19/elements-1.jpg',
+    link: 'https://images.unsplash.com/photo-1450778869180-41d0601e046e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1886&q=80',
     alt: 'белая собака и рыжая кошка лежат на траве'
   },
   {
     name: 'Дэйзи',
-    link: 'https://ltdfoto.ru/images/2023/07/19/elements-2.jpg',
+    link: 'https://images.unsplash.com/photo-1618718547092-8c8927bb8d4e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80',
     alt: 'щенок засыпает в голубом пледе'
   },
   {
     name: 'Самсон',
-    link: 'https://ltdfoto.ru/images/2023/07/19/elements-3.jpg',
+    link: 'https://images.unsplash.com/photo-1583160247711-2191776b4b91?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2071&q=80',
     alt: 'собака сидит среди желтых цветов'
   },
   {
     name: 'Лаки',
-    link: 'https://ltdfoto.ru/images/2023/07/19/elements-4.jpg',
+    link: 'https://images.unsplash.com/photo-1578362613743-cb09ce05275d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80',
     alt: 'щенок овчарки отдыхает'
   },
   {
     name: 'Рокси',
-    link: 'https://ltdfoto.ru/images/2023/07/19/elements-5.jpg',
+    link: 'https://images.unsplash.com/photo-1503595855261-9418f48a991a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80',
     alt: 'черная собака зевает'
   },
   {
     name: 'Ханни',
-    link: 'https://ltdfoto.ru/images/2023/07/19/elements-6.jpg',
+    link: 'https://images.unsplash.com/photo-1600804340584-c7db2eacf0bf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80',
     alt: 'щенок в цветном ошейнике улыбается'
   }
 ]; 
 
-const closeButtons = Array.from(document.querySelectorAll('.popup__close-button'))
+const buttonsClose = Array.from(document.querySelectorAll('.popup__close-button'))
 const cardElements = document.querySelector('.elements')
 const cardTemplate = document.querySelector('#card-template').content
 const cardForm = document.querySelector('.card-form')
-const popupLink = document.querySelector('.popup__input_type_link')
-const popupTitle = document.querySelector('.popup__input_type_title')
+const inputLink = document.querySelector('.popup__input_type_link')
+const inputTitle = document.querySelector('.popup__input_type_title')
 const popupBigPicture = document.querySelector('.popup_type_big-picture')
 
-
-function openPopupEdit() {
-    popupEdit.classList.add('popup_opened')
-    popupProfileName.value = profileName.textContent
-    popupAbout.value = profileDescription.textContent
+function openPopup(popup) {
+  popup.classList.add('popup_opened')
 }
 
-function openPopupAdd() {
-  popupAdd.classList.add('popup_opened')
+function closePopup(popup) {
+  popup.classList.remove('popup_opened')
 }
 
 function savePopup (evt) {
   evt.preventDefault()
-  profileName.textContent = popupProfileName.value
-  profileDescription.textContent = popupAbout.value
-  popupEdit.classList.remove('popup_opened')
+  profileName.textContent = inputProfileName.value
+  profileDescription.textContent = inputAbout.value
+  closePopup(popupEditProfile)
 }
 
 function deleteCard (evt) {
@@ -72,71 +69,73 @@ function deleteCard (evt) {
   element.remove()
 }
 
-function like () {
-  const heartIcons = Array.from(document.querySelectorAll('.heart-icon'))
-  heartIcons.forEach(function(item) {
-    item.addEventListener ('click', function() {
-      item.classList.add('heart-icon_active')
-    })
-  })
+function like(evt) {
+  evt.target.classList.toggle('heart-icon_active')
 } 
 
-function renderItem (array) {
+function renderCard(card, container) {
+  container.prepend(card);
+}
+
+function createCard (array) {
   const cardElement = cardTemplate.querySelector('.element').cloneNode(true) 
+  const cardElementImage = cardElement.querySelector('.element__image')
      cardElement.querySelector('.element__title').textContent = array.name
-     cardElement.querySelector('.element__image').src = array.link
-     cardElement.querySelector('.element__image').alt = array.alt
-     cardElements.prepend(cardElement)
+     cardElementImage.src = array.link
+     cardElementImage.alt = array.alt
      cardElement.querySelector('.trash-icon').addEventListener('click', deleteCard)
       
-     like ()
+     const cardLike = cardElement.querySelector('.heart-icon');
+    cardLike.addEventListener('click', like); 
      
      const cardImage = cardElement.querySelector('.element__image')
      cardImage.addEventListener ('click', function () {
-      popupBigPicture.classList.add('popup_opened')
+      openPopup(popupBigPicture)
       popupBigPicture.querySelector('.big-picture').src = array.link
       popupBigPicture.querySelector('.big-picture-title').textContent = array.name
+      popupBigPicture.querySelector('.big-picture').alt = array.alt
       popupBigPicture.style.background = 'rgba(0, 0, 0, 0.8)'
     })
+    return cardElement
     
 }
 
-closeButtons.forEach(function(item) {
+buttonsClose.forEach(function(item) {
   item.addEventListener('click', function () {
     const popup = item.closest('.popup')
-    popup.classList.remove('popup_opened')
+    closePopup(popup)
   })
  })
 
-
-addButton.addEventListener('click', openPopupAdd)
+buttonAdd.addEventListener('click', function() {
+  openPopup(popupAddCard)
+})
 
 cardForm.addEventListener ('submit', (evt) => {
   evt.preventDefault ()
    const newCard = {
-    name: popupTitle.value,
-    link: popupLink.value 
+    name: inputTitle.value,
+    link: inputLink.value,
+  alt: inputTitle.value
   }
-   renderItem(newCard) 
-   popupAdd.classList.remove('popup_opened')
+
+inputTitle.value=''
+inputLink.value=''
+
+   renderCard(createCard(newCard), cardElements)
+   closePopup(popupAddCard)
+   
 })
 
-
-
-
-
-
-
-
-
-
 initialCards.forEach(function (item) {
- renderItem(item)
-   
-  })
+  const card = createCard(item)
+  renderCard(card, cardElements)
+   })
 
-  editButton.addEventListener('click', openPopupEdit) 
-    editForm.addEventListener ('submit', savePopup)
+  popupEditButton.addEventListener('click', function () {
+    openPopup(popupEditProfile)
+    inputProfileName.value = profileName.textContent
+    inputAbout.value = profileDescription.textContent
 
-
-
+  }) 
+    formEdit.addEventListener ('submit', savePopup)
